fcae62e253d65b18287504acf088c78cae6dbb4e
mamekin05108 <nts05108@gmail.com>
2026-06-07 17:42:12 +0900

    仮バージョン

---
+++ Hensai/serializers.py
@@ -0,0 +1,13 @@
+# testapp/serializers.py (もしくは該当する serializers.py)
+from rest_framework import serializers
+
+
+class CustomerQuerySerializer(serializers.Serializer):
+    shop_id = serializers.CharField(max_length=100)
+    customer_id = serializers.CharField(max_length=100)
+    # 送信先選択用のフィールドを追加（デフォルト値は 'none'）
+    action_type = serializers.ChoiceField(
+        choices=[("A", "A先"), ("B", "B先"), ("none", "保存のみ")],
+        default="none",
+        required=False,
+    )
